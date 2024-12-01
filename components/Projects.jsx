67c79d700fe0e351faac5699b8163458c95b6e3e
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-01 22:11:30 +0700

    add project arr

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,88 @@
 import React from 'react'
 
+const project = [
+  {
+    image: ['/projects/citycargo.jpg'],
+    category: ['Codeigniter 4', 'Bootstrap 5'],
+    name: 'City Cargo Landing Page',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: 'https://citycargo.id',
+    github: ''
+  },
+  {
+    image: ['/projects/ccbot1.jpg', '/projects/ccbot2.jpg'],
+    category: ['Codeigniter 4', 'Bootstrap 5'],
+    name: 'City Cargo Telegram Bot App',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: 'https://t.me/CityCargo_bot',
+    github: ''
+  },
+  {
+    image: ['/projects/admincc.jpg'],
+    category: ['Codeigniter 4', 'Bootstrap 5'],
+    name: 'City Cargo Admin Page',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '',
+    github: ''
+  },
+  {
+    image: ['/projects/abisha.jpg'],
+    category: ['Codeigniter 4'],
+    name: 'Abisha',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: 'https://abisha.id',
+    github: ''
+  },
+  {
+    image: ['/projects/mmsi.jpg'],
+    category: ['Codeigniter 4'],
+    name: 'MMSI Landing Page',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: 'https://mms.id',
+    github: ''
+  },
+  {
+    image: ['/projects/adminmmsi.jpg'],
+    category: ['Laravel 11'],
+    name: 'MMSI Admin Page',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '',
+    github: ''
+  },
+  {
+    image: ['/projects/pantaucorona.jpg'],
+    category: ['Flutter'],
+    name: 'Pantau Corona',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '',
+    github: 'https://github.com/mrizkinm/pantaucorona'
+  },
+  {
+    image: ['/projects/laporcepat1.jpg', '/projects/laporcepat2.jpg', '/projects/laporcepat3.jpg'],
+    category: ['Flutter'],
+    name: 'Lapor Cepat',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    link: '',
+    github: 'https://github.com/mrizkinm/laporcepat'
+  },
+  {
+    image: ['/projects/pantaucorona.jpg'],
+    category: ['Django'],
+    name: 'Food price prediction application uses ARIMA method',
+    description: 'Food price prediction application for basic needs uses the ARIMA (Autoregressive Integrated Moving Average) method with the django python framework',
+    link: '',
+    github: 'https://github.com/mrizkinm/prediksi'
+  },
+  {
+    image: ['/projects/pantaucorona.jpg'],
+    category: ['Codeigniter 3'],
+    name: 'SPK SAW method for selecting cashier employees',
+    description: 'SPK SAW method for selecting cashier employees',
+    link: '',
+    github: 'https://github.com/mrizkinm/spksawkasir'
+  }
+];
+
 const Projects = () => {
   return (
     <section id="projects">Projects</section>
