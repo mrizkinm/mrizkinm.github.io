216df914e3fbe7becc15510dc01a7c26e11e8071
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-03 21:50:40 +0700

    fix card height

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -4,24 +4,23 @@ import React from 'react'
 import { Card, CardHeader } from './ui/card'
 import { Github, Link2Icon } from 'lucide-react'
 import { Badge } from './ui/badge'
-import { Button } from './ui/button'
 import ImageModal from './ImageModal'
 
 const ProjectCard = ({project}) => {
   return (
-    <Card className="group overflow-hidden relative">
+    <Card className="group overflow-hidden h-full relative">
       <CardHeader className="p-0">
-        <div className="relative w-full h-[250px] flex items-center justify-center bg-green-50 dark:bg-secondary/40">
+        <div className="relative w-full h-[250px] flex items-center justify-center bg-blue-50 dark:bg-secondary/40">
           <Image className="absolute bottom-0 shadow-2xl object-contain w-full h-full" src={project.image[0]} alt="Image" layout="fill" priority />
           <div className="flex gap-x-4">
             <ImageModal images={project.image} />
             {project.link && (
-              <Link href={project.link} target="_blank" className="bg-slate-700 w-[45px] h-[45px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200">
+              <Link href={project.link} target="_blank" title="Url" className="bg-slate-700 w-[45px] h-[45px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200">
               <Link2Icon className="text-white" />
             </Link>
             )}
             {project.github && (
-              <Link href={project.github} target="_blank" className="bg-slate-700 w-[45px] h-[45px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200">
+              <Link href={project.github} target="_blank" title="Github" className="bg-slate-700 w-[45px] h-[45px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200">
                 <Github className="text-white" />
               </Link>
             )}
