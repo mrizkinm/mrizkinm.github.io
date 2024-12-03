59d239bad26eed4891ce89f4ab4ad6ff42e4e000
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-03 22:48:46 +0700

    update hover button

--- components/ThemeButton.jsx
+++ components/ThemeButton.jsx
@@ -9,7 +9,7 @@ const ThemeButton = () => {
   const {theme, setTheme} = useTheme()
 
   return (
-    <Button variant="outline" size="icon" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className="rounded-full p-3">
+    <Button className="hover:bg-transparent hover:shadow-none hover:text-inherit focus:ring-0 active:bg-transparent" variant="outline" size="icon" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className="rounded-full p-3">
       <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
     </Button>
