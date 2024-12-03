d9c10c4e07b925fa2af64e439e2a75a2d4817f3d
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-03 22:18:04 +0700

    add justify

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -34,7 +34,7 @@ const ProjectCard = ({project}) => {
           ))}
         </div>
         <h4 className="h4 mb-1">{project.name}</h4>
-        <p className="text-muted-foreground text-lg">{project.description}</p>
+        <p className="text-muted-foreground text-lg text-justify">{project.description}</p>
       </div>
     </Card>
   )
