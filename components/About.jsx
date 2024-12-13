a74ca1abf2d3dd23986b9b329cb6dd65bee157fb
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-13 15:00:03 +0700

    update bio

--- components/About.jsx
+++ components/About.jsx
@@ -90,7 +90,7 @@ const About = () => {
                 <TabsContent value="personal">
                   <div>
                     <h3 className="h3 mb-8">Hello, I&apos;am Muhammad Rizki! 👋</h3>
-                    <p className="subtitle text-justify">I&apos;am an experienced Full Stack Developer with more than 7 years in the software development industry. Graduating from SMK Negeri 1 Surabaya majoring in Software Engineering in 2017, I continued my education at Bhayangkara University Surabaya starting in 2018 while working, and successfully earned my bachelor's degree in 2023.</p>
+                    <p className="subtitle text-justify">I&apos;am an experienced Full Stack Developer with more than 7 years in the software development industry. Graduating from SMK Negeri 1 Surabaya majoring in Software Engineering in 2017, I continued my education at Bhayangkara University Surabaya starting in 2018 while working, and successfully earned my bachelor&apos;s degree in 2023.</p>
                     <p className="subtitle text-justify">With expertise in frontend and backend, I&apos;am able to develop modern, intuitive websites, as well as develop efficient support systems. In addition, I also have experience in building Android mobile apps, providing end-to-end solutions for web and mobile-based projects.</p>
                     <p className="subtitle text-justify">I love working with people to bring their ideas to life. If you need a website or Android app, let&apos;s connect and create something amazing together!.</p>
                     <div className="grid gap-4 mb-12">
