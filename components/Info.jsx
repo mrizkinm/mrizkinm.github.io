c5999fcb83eeb676ed1aec6c32ba64010f78c9d0
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2024-12-16 10:46:45 +0700

    minor fix

--- components/Info.jsx
+++ components/Info.jsx
@@ -13,8 +13,8 @@ const Info = () => {
             <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-widest">
               Let&apos;s Work Together
             </div>
-            <h1 className="h1 mb-4">Hello, I&apos;am Muhammad Rizki! 👋</h1>
-            <p className="subtitle max-w-[500px] mx-auto xl:mx-0">I&apos;am a Full Stack Developer specializing in building both frontend and backend web applications, as well as Android mobile applications</p>
+            <h1 className="h1 mb-4">Hello, I am Muhammad Rizki! 👋</h1>
+            <p className="subtitle max-w-[500px] mx-auto xl:mx-0">I am a Full Stack Developer specializing in building both frontend and backend web applications, as well as Android mobile applications</p>
             <div className="mx-auto xl:mx-0">
               <Socials />
             </div>
